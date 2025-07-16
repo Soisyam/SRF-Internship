c86dc3782137981d42fb66eb5f70bc17a56dc8f6
Soyam <soiamrai88@gmail.com>
2025-07-16 17:48:09 +0530

    Fix vite.config.ts Tailwind CSS plugin import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // <-- Changed this line
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
 })
